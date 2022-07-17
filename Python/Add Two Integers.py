474466b0d6e41e9c4c2591d5afd1efeb44a4defe
hamid <hamidjahani999@gmail.com>
2022-07-17 15:58:39 +0430

    add python code for (Add Two Integers) (number : 2235)

---
+++ Python/Add Two Integers.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1+num2
